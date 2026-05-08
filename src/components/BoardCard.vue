d4470140048591edaf419b84355754c7f1f3eedf
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2026-05-08 23:57:41 +0200

    fix board card

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -13,12 +13,17 @@ defineEmits<{ flip: [] }>();
         ? `Karta ${index + 1}, odkrytá: ${card.alt}`
         : `Karta ${index + 1}, skrytá`
     "
-    :class="{ 'rotate-y-180': card.isFlipped || card.isMatched }"
-    class="aspect-square cursor-pointer rounded bg-blue-500 transition-transform duration-300 focus:ring-4 focus:ring-blue-800 focus:outline-none disabled:cursor-default"
+    class="group aspect-square cursor-pointer rounded bg-transparent perspective-normal focus:ring-4 focus:ring-blue-800 focus:outline-none disabled:cursor-default"
     @click="$emit('flip')"
   >
-    <figure v-if="card.isFlipped || card.isMatched" class="size-full">
-      <img :src="card.imgUrl" :alt="card.alt" class="size-full rounded object-cover shadow-md" />
-    </figure>
+    <div
+      :class="{ 'rotate-y-180': card.isFlipped || card.isMatched }"
+      class="relative size-full transition-transform duration-500 transform-3d"
+    >
+      <div class="absolute inset-0 rounded bg-blue-500 shadow-md backface-hidden"></div>
+      <figure class="absolute inset-0 rotate-y-180 backface-hidden">
+        <img :src="card.imgUrl" :alt="card.alt" class="size-full rounded object-cover shadow-md" />
+      </figure>
+    </div>
   </button>
 </template>
