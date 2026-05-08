4e6c8ed8cfdcd6db8273e6e343c230842fd8384e
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2026-05-08 23:21:17 +0200

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Config from '@/pages/config.vue';
 import Index from '@/pages/index.vue';
+import Play from '@/pages/play.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Index', component: Index },
+    { path: '/', name: 'Overview', component: Index },
     { path: '/config', name: 'Game config', component: Config },
+    { path: '/play', name: 'Game', component: Play },
   ],
 });
 
