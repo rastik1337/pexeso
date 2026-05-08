586e15dcfebad55f7ffe5a9423833c8dd956dac5
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2026-05-08 23:24:45 +0200

    add request splitting and return ratelimit info

--- netlify/functions/getImages.ts
+++ netlify/functions/getImages.ts
@@ -1,9 +1,10 @@
 import type { Handler, HandlerEvent } from '@netlify/functions';
 
 export const handler: Handler = async (event: HandlerEvent) => {
-  const theme = event.queryStringParameters?.theme || 'nature';
-  const count = event.queryStringParameters?.count || '8';
+  const topic = event.queryStringParameters?.topic || 'nature';
   const token = process.env.API_UNSPLASH;
+  const baseUrl = 'https://api.unsplash.com';
+  const endpoint = '/photos/random';
 
   if (!token) {
     return {
@@ -13,19 +14,48 @@ export const handler: Handler = async (event: HandlerEvent) => {
   }
 
   try {
-    const response = await fetch(
-      `https://api.unsplash.com/photos/random?count=${count}&query=${theme}&client_id=${token}`,
-    );
+    const requests: Promise<unknown>[] = [];
+    let remainingImages = parseInt(event.queryStringParameters?.count || '8', 10);
+    let remainingRequests = 50;
 
-    if (!response.ok) {
-      throw new Error(`Unsplash API error: ${response.status}`);
+    while (remainingImages > 0) {
+      const fetchCount = Math.min(remainingImages, 30);
+      const params = new URLSearchParams({
+        query: topic,
+        count: fetchCount.toString(),
+        client_id: token,
+      });
+      const handler = async (r: Response) => {
+        if (!r.ok) throw new Error(`Unsplash API error: ${r.status}`);
+        const limitHeader = r.headers.get('x-ratelimit-remaining');
+        if (limitHeader) {
+          const limit = parseInt(limitHeader, 10);
+          if (limit < remainingRequests) {
+            remainingRequests = limit;
+          }
+        }
+        return r.json();
+      };
+      requests.push(fetch(`${baseUrl}${endpoint}?${params.toString()}`).then(handler));
+      remainingImages -= fetchCount;
     }
 
-    const data = await response.json();
-
+    const results = (await Promise.all(requests)).flat();
+    const imageMap = new Map(
+      results.map((img) => {
+        const isObject = typeof img === 'object' && img !== null;
+        if (!isObject || !('id' in img)) {
+          throw new TypeError('Received items in unexpected format');
+        }
+        return [img.id, img];
+      }),
+    );
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify({
+        images: imageMap.values().toArray(),
+        remainingRequests,
+      }),
     };
   } catch (error) {
     console.error('Error fetching images:', error);
